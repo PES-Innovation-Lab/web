cc6cd9cf99901bd115c4ee23f99d447052a4d4b5
souravtecken <souravtecken@gmail.com>
2019-08-27 23:54:44 +0530

    index/js/css/pages/images: Updated home page and several minor changes -> Added professor images -> Added events page -> Complete home page overhaul Co-authored-by: Sparsh Temani <sparshtemani31415@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-});
-
-// Or with jQuery
-
-$(document).ready(function(){
-    $('.parallax').parallax();
-});
+(function ($) {
+    $(function () {
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
