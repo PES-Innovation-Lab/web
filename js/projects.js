de3b7f3d3d14b1759a44fcc92e434de927866292
Sumanth V Rao <sumanthvrao@gmail.com>
2019-09-14 09:22:40 +0530

    pages/js: Added 2016 projects to the cyclic carousel.
    * Added html to order the 2016 projects after 2017 projects.
      This cyclic carries on to 2019 on the right.
    
    * Added line to js for DOM to render 2016 projects as well.

--- js/projects.js
+++ js/projects.js
@@ -18,23 +18,23 @@ const ProjectCard = (props) => { //"unique" attribute in props to associate each
                     <div class="card-image">
                         {
                             props.project.imageURL ? <img src={props.project.imageURL}/> : <img src="../images/mlab/no_project_image.png"/>
-                        }                        
+                        }
                         <a class="btn-floating halfway-fab waves-effect waves-light green modal-trigger" href={'#'+props.unique}><i class="material-icons">help_outline</i></a>
                     </div>
-                    <div class="card-content">                        
-                        <span class="card-title grey-text activator text-darken-4"><i class="material-icons left">more_vert</i>{props.project.Name}</span>                                                
+                    <div class="card-content">
+                        <span class="card-title grey-text activator text-darken-4"><i class="material-icons left">more_vert</i>{props.project.Name}</span>
                     </div>
                     <div class="card-reveal">
                         <blockquote class="card-title grey-text text-darken-4">{props.project.Name}<i class="material-icons right">close</i></blockquote>
                         <p class="card-title grey-text text-darken-4">{props.project["short summary"]}</p>
                         <br></br>
                         <h5 align="left" class="grey-text text-darken-3">Mentors</h5>
-                        <ul align="left" className="mentor-list">                            
-                            {props.project.mentors.split(",").map((mentor) => <li class="card-title grey-text text-darken-3">{mentor}</li>)}                            
+                        <ul align="left" className="mentor-list">
+                            {props.project.mentors.split(",").map((mentor) => <li class="card-title grey-text text-darken-3">{mentor}</li>)}
                         </ul>
                         <h5 align="left" class="grey-text text-darken-3">Interns</h5>
-                        <ul align="left" className="intern-list">                            
-                            {props.project.interns.split(",").map((intern) => <li class="card-title grey-text text-darken-3">{intern}</li>)}                            
+                        <ul align="left" className="intern-list">
+                            {props.project.interns.split(",").map((intern) => <li class="card-title grey-text text-darken-3">{intern}</li>)}
                         </ul>
                     </div>
                 </div>
@@ -61,12 +61,12 @@ class ProjectsContainer extends React.Component {
             })
             .catch(function(err){
                 console.log(err);
-            })               
+            })
     }
     componentDidUpdate(){
-        $('.modal').modal();        
+        $('.modal').modal();
     }
-    render(){                
+    render(){
         let body = <h2 align="center" style={{"color":"white", "margin-top":"200px"}}>Coming soon...</h2>;
         if(this.state.projects){
             /*
@@ -86,16 +86,17 @@ class ProjectsContainer extends React.Component {
                         {projectRow}
                     </div>
                 )
-            }            
+            }
         }
         return(
             <div class="row" style={{"marginBottom":"40px"}}>
                 {projectsInRows}
-            </div>            
-        )        
+            </div>
+        )
     }
 }
 
 ReactDOM.render (<ProjectsContainer year="2019"/>, document.getElementById("projects2019"));
 ReactDOM.render (<ProjectsContainer year="2018"/>, document.getElementById("projects2018"));
 ReactDOM.render (<ProjectsContainer year="2017"/>, document.getElementById("projects2017"));
+ReactDOM.render (<ProjectsContainer year="2016"/>, document.getElementById("projects2016"));
