68d9037011cdbc7330625baf7cc9bfc0783e2da1
souravtecken <souravtecken@gmail.com>
2019-09-16 09:39:48 +0530

    events/projects: Moved content to modal, modified events view. 1. projects.js - Moved all content to modal. 2. Updated events page, removed cards. 3. Made event images white.

--- js/projects.js
+++ js/projects.js
@@ -4,17 +4,31 @@ const ProjectCard = (props) => { //"unique" attribute in props to associate each
     return (
         <Aux>
             <div id={props.unique} class="modal modal-fixed-footer">
-                <div class="modal-content">
-                    <h4>{props.project.Name}</h4>
-                    <br></br>
-                    <blockquote className="project_description" align="left">{props.project["Project description"]}</blockquote>
+                <div class="modal-content">                                        
+                    <h4 class="card-title grey-text text-darken-4">{props.project.Name}<i class="modal-close material-icons right">close</i></h4>
+                    <p class="card-title grey-text text-darken-4">{props.project["short summary"]}</p>                    
+                    <blockquote className="project_description" align="left">{props.project["Project description"]}</blockquote>                                        
+                    <div className="row">
+                        <div className="col m6">
+                            <h5 align="center" class="grey-text text-darken-3">Mentors</h5>
+                            <ul align="center" className="mentor-list">
+                                {props.project.mentors.split(",").map((mentor) => <li class="card-title grey-text text-darken-3">{mentor}</li>)}
+                            </ul>
+                        </div>
+                        <div className="col m6">
+                            <h5 align="center" class="grey-text text-darken-3">Interns</h5>
+                            <ul align="center" className="intern-list">
+                                {props.project.interns.split(",").map((intern) => <li class="card-title grey-text text-darken-3">{intern}</li>)}
+                            </ul>
+                        </div>
+                    </div>                                        
                 </div>
                 <div class="modal-footer">
-                    <a href="#!" class="modal-close waves-effect waves-red btn-flat">Close</a>
+                    <a href="#!" className="modal-close waves-effect waves-red btn-flat">Close</a>
                 </div>
             </div>
             <div class="col m4">
-                <div class="card">
+                <div class="card modal-trigger" href={'#'+props.unique}>
                     <div class="card-image">
                         {
                             props.project.imageURL ? <img src={props.project.imageURL}/> : <img src="../images/mlab/no_project_image.png"/>
@@ -22,21 +36,8 @@ const ProjectCard = (props) => { //"unique" attribute in props to associate each
                         <a class="btn-floating halfway-fab waves-effect waves-light green modal-trigger" href={'#'+props.unique}><i class="material-icons">help_outline</i></a>
                     </div>
                     <div class="card-content">
-                        <span class="card-title grey-text activator text-darken-4"><i class="material-icons left">more_vert</i>{props.project.Name}</span>
-                    </div>
-                    <div class="card-reveal">
-                        <blockquote class="card-title grey-text text-darken-4">{props.project.Name}<i class="material-icons right">close</i></blockquote>
-                        <p class="card-title grey-text text-darken-4">{props.project["short summary"]}</p>
-                        <br></br>
-                        <h5 align="left" class="grey-text text-darken-3">Mentors</h5>
-                        <ul align="left" className="mentor-list">
-                            {props.project.mentors.split(",").map((mentor) => <li class="card-title grey-text text-darken-3">{mentor}</li>)}
-                        </ul>
-                        <h5 align="left" class="grey-text text-darken-3">Interns</h5>
-                        <ul align="left" className="intern-list">
-                            {props.project.interns.split(",").map((intern) => <li class="card-title grey-text text-darken-3">{intern}</li>)}
-                        </ul>
-                    </div>
+                        {props.project.Name}
+                    </div>                    
                 </div>
             </div>
         </Aux>
