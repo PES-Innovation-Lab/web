452c03bee76c3da58006209b04c1dc54eb3d795e
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2020-09-16 19:27:09 +0530

    design: Moved background css to Layout from index.css

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,6 @@ import NavBar from "./NavBar";;
 import Footer from "./Footer";
 import Head from "next/head";
 
-
 const layoutStyle = {
     display: "flex",
     flexDirection: "column",
@@ -16,11 +15,36 @@ const contentStyle = {
     flexDirection: "column"
 };
 
-const Layout = (props) => (
+
+function Layout(props){
+    const background_img = `${process.env.ASSET_PREFIX}/images/mlab/prism.png`;
+    const intro_img = `${process.env.ASSET_PREFIX}/images/mlab/intro.jpg`;
+    const hws_img = `${process.env.ASSET_PREFIX}/images/mlab/hws.jpg`;
+    return(
     <div style={layoutStyle}>
         <Head>
             <title>{props.title}</title>
             <link rel="shortcut icon" href={`${process.env.ASSET_PREFIX}/images/favicon.ico`} />
+            <style>{`
+                html, body {
+                    margin: 0;
+                    padding: 0;
+                    background-image: url(${background_img});
+                    background-repeat: repeat;
+                    min-height: 100vh;
+                }
+                .introSection {
+                    background-image: url(${intro_img});
+                    clip-path: polygon(0% 0%, 100% 0%, 100% 90%, 0% 100%);
+                }
+                .statSection {
+                    background-image: url(${hws_img});
+                    padding: 70px 0px;
+                    clip-path: polygon(0% 20%, 100% 0%, 100% 80%, 0% 100%);
+                }
+                                
+            `}</style>
+        
         </Head>
         <NavBar active={props.active} search={props.search} searchSettings={props.searchSettings} />
         <div style={layoutStyle}>
@@ -28,6 +52,7 @@ const Layout = (props) => (
         </div>
         <Footer/>
     </div>
-);
+    );
+}
 
 export default Layout;
