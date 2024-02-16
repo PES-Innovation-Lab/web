8f5f8480c4a9ba5b3ec384689e17c20b69610704
Anshul Dasyam <pesanshul@gmail.com>
2024-02-16 14:00:00 +0530

    We have terrible commit messages sorry :/ but here's your answer: INTERNSHIP

--- next.config.js
+++ next.config.js
@@ -5,16 +5,20 @@ const isProduction = (process.env.NODE_ENV || 'production') === 'production';
 const assetPrefix = isProduction ? '' : '';
 
 module.exports = {
-  trailingSlash: true,
-  // assetPrefix: assetPrefix,
-  webpack: (config) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
-      })
-    );
-    return config;
-  },
-  swcMinify: true,
-  images: { domains: ['pil-web.web.app'] },
+	trailingSlash: true,
+	// assetPrefix: assetPrefix,
+	webpack: (config) => {
+		config.plugins.push(
+			new webpack.DefinePlugin({
+				'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+			})
+		);
+		return config;
+	},
+	swcMinify: true,
+	images: {
+		domains: ['pil-web.web.app', 'theinnovationlab.in'],
+		loader: 'akamai',
+		path: ''
+	},
 };
