1810e328fa53850260d445ac4ea7456329e53cf1
manasg1111 <118659036+manasg1111@users.noreply.github.com>
2024-10-14 20:05:36 +0530

    Updated members page, some UI and logic changes
    * Updated project details 2024
    * Updated project details 2024

--- app/members/page.jsx
+++ app/members/page.jsx
@@ -6,9 +6,10 @@ import MemberCard from '../../components/MemberCard'
 import LayoutAlt from '../../components/LayoutAlt'
 
 function Members() {
-  const [currentYear, setCurrentYear] = useState(2024)
+
   const [showYears, setShowYears] = useState(false)
   const years = [2024, 2023, 2022, 2020, 2019, 2018, 2017, 2016, 2015]
+  const [currentYear, setCurrentYear] = useState(years.sort((a, b) => { b - a })[0])
   const yearElementsMd = years.map((year, ind) => {
     return (
       <button
@@ -34,38 +35,12 @@ function Members() {
   return (
     <>
       <div className="mt-10 flex flex-col gap-10 px-12 pb-20 pt-20">
-        <div className="flex items-center justify-between">
+        <div className="flex md:flex-row flex-col items-center gap-8 justify-between">
           <div className="font-martian-mono md:text-4xl text-3xl text-lab-green">
             MEMBERS
           </div>
-          <div className="gap-4 rounded-md border-2 border-lab-green p-2 text-lg text-gray-400 md:flex hidden">
+          <div className="md:gap-4 gap-2 rounded-md border-2 border-lab-green p-2 text-lg text-gray-400 flex flex-wrap md:flex-row justify-center ">
             {yearElementsMd}
-          </div>
-          <div className="relative md:hidden">
-            <div className="flex gap-4 items-center rounded-md border-2 border-lab-green p-2 text-lg text-gray-400 cursor-pointer">
-              <button
-                className="text-gray-400 text-xs px-4 py-1"
-                onClick={() => setShowYears(!showYears)}
-              >
-                {currentYear}
-              </button>
-            </div>
-            {showYears && (
-              <div className="absolute left-0 right-0 mt-2 bg-black shadow-lg rounded-md border-2 border-lab-green z-20 transition-all duration-300 ease-in-out max-h-48 overflow-y-auto">
-                {years.map((year, ind) => (
-                  <button
-                    key={ind}
-                    onClick={() => {
-                      setCurrentYear(year);
-                      setShowYears(false);
-                    }}
-                    className={`block w-full text-center py-2 text-xs ${year === currentYear ? 'text-green-600' : 'text-gray-400'} hover:bg-gray-100`}
-                  >
-                    {year}
-                  </button>
-                ))}
-              </div>
-            )}
           </div>
         </div>
       </div>
